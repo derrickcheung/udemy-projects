54cd27f146b1a396cc52cf5546dc6d104bce11aa
derrickcheung <derrickcheung@users.noreply.github.com>
2025-04-24 13:47:17 -0700

    first commit

---
+++ Section 18/Drum Kit Starting Files/index.js
@@ -0,0 +1,8 @@
+document.querySelector(".set").addEventListener("click", handleClick)
+
+function handleClick(event) {
+    if (event.target.tagName === "BUTTON") {
+        alert("I got clicked");
+    }
+    
+}
