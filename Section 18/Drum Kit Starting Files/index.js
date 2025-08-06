851114c8704e3f75458f804ad7e92dcacb962d24
derrickcheung <derrickcheung@users.noreply.github.com>
2025-08-06 00:28:43 -0700

    x

--- Section 18/Drum Kit Starting Files/index.js
+++ Section 18/Drum Kit Starting Files/index.js
@@ -2,9 +2,42 @@ document.querySelector(".set").addEventListener("click", handleClick)
 
 function handleClick(event) {
     if (event.target.tagName === "BUTTON") {
-        var audio = new Audio("./sounds/tom-1.mp3");
-        audio.play();
+        // var audio = new Audio("./sounds/tom-1.mp3");
+        // audio.play();
+        // console.log(event.target.style.color ="white")
+        var buttonInnerHTML = event.target.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio("./sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio("./sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                var tom3 = new Audio("./sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio("./sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                var snare = new Audio("./sounds/snare.mp3");
+                snare.play();
+                break;
+            case "k":
+                var kickbass = new Audio("./sounds/kick-bass.mp3");
+                kickbass.play();
+            break;
+            case "l":
+                var crash = new Audio("./sounds/crash.mp3");
+                crash.play();
+                break;
+            default: console.log();
+                
+        }
     }
     
 }
-
