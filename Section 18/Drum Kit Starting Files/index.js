7ba211f8ea47a1cd4aa2f9a2883558f197caf491
derrickcheung <derrickcheung@users.noreply.github.com>
2025-08-05 21:19:43 -0700

    Update index.js

--- Section 18/Drum Kit Starting Files/index.js
+++ Section 18/Drum Kit Starting Files/index.js
@@ -2,7 +2,8 @@ document.querySelector(".set").addEventListener("click", handleClick)
 
 function handleClick(event) {
     if (event.target.tagName === "BUTTON") {
-        alert("I got clicked");
+        var audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
     }
     
 }
